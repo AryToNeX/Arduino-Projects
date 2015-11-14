fac0b9bbb14e191c58ca09dd90ef2ad08c6d2f9a
Anthony <tony0000.ac@gmail.com>
2015-11-14 13:24:50 +0100

    SOS Morse Code in a Led on PIN 13

---
+++ sosled/sosled.ino
@@ -0,0 +1,46 @@
+// Made by @AryToNeX
+
+const int led = 13;
+
+void setup(){
+  pinMode(led,OUTPUT);
+}
+
+void loop(){
+  digitalWrite(led,HIGH);
+  delay(100);
+  digitalWrite(led,LOW);
+  delay(400);
+  digitalWrite(led,HIGH);
+  delay(100);
+  digitalWrite(led,LOW);
+  delay(400);
+  digitalWrite(led,HIGH);
+  delay(100);
+  digitalWrite(led,LOW);
+  delay(400);
+  digitalWrite(led,HIGH);
+  delay(400);
+  digitalWrite(led,LOW);
+  delay(100);
+  digitalWrite(led,HIGH);
+  delay(400);
+  digitalWrite(led,LOW);
+  delay(100);
+  digitalWrite(led,HIGH);
+  delay(400);
+  digitalWrite(led,LOW);
+  delay(100);
+  digitalWrite(led,HIGH);
+  delay(100);
+  digitalWrite(led,LOW);
+  delay(400);
+  digitalWrite(led,HIGH);
+  delay(100);
+  digitalWrite(led,LOW);
+  delay(400);
+  digitalWrite(led,HIGH);
+  delay(100);
+  digitalWrite(led,LOW);
+  delay(400);
+}
