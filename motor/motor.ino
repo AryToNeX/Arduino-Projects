2bfabb7088237a9f6a28dbc5918a26ce13f5c4f3
Anthony <tony0000.ac@gmail.com>
2015-11-14 13:21:23 +0100

    Motor.ino! @SantX27

---
+++ motor/motor.ino
@@ -0,0 +1,21 @@
+// Code by @AryToNeX
+
+const int motorA = 2;
+const int motorB = 3;
+
+void setup(){
+  pinMode(motorA,OUTPUT);
+  pinMode(motorB,OUTPUT);
+}
+
+void loop(){
+  digitalWrite(motorA,HIGH);
+  digitalWrite(motorB,LOW);
+  delay(5000);
+  digitalWrite(motorA,LOW);
+  delay(1000);
+  digitalWrite(motorB,HIGH);
+  delay(5000);
+  digitalWrite(motorB,LOW);
+  delay(1000);
+}
